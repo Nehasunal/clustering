4b3c6be242da4fb423bb44401b4d87c4af5e9c36
neha.sunal <nehasunal04@gmail.com>
2024-02-05 11:23:00 +0530

    add worker thread

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 const { parentPort } = require('worker_threads');
 
-// Simulate a heavy computation task
+// add heavy computation task
 function performHeavyComputation() {
     let sum = 0;
     for (let i = 0; i < 1e9; i++) {
