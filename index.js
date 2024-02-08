0bc6084cd8524a191eefc3b4dd7e132060bd95fc
neha.sunal <neha.sunal@kellton.com>
2024-02-08 10:12:00 +0530

    feat: clustering

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+const cluster = require('cluster');
+const os = require('os');
 const express = require('express');
-const app = express();
 
+const PORT = process.env.PORT || 3000;
 
-function doWork(duration){  
-    // this function will block our event loop for 5 sec, 
-    // it wont let us execute other function or task.
-    // hit localhost in 2 diff tabs, second will give response once first 5 sec is completed.
-    // Clustering will solve this issue.
-    const start = Date.now();
-    while(Date.now() - start < duration){}
+if (cluster.isMaster) {
+    // Get the number of CPU cores
+    const numCPUs = os.cpus().length;
+
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers based on the number of CPU cores
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    // If a worker dies, start a new worker
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+
+} else {
+    // This block runs for each worker
+
+    const app = express();
+
+    // Middleware to log which worker is handling the request
+    app.use((req, res, next) => {
+        console.log(`Worker ${process.pid} is handling the request`);
+        next();
+    });
+
+    // Define a simple route
+    app.get('/', (req, res) => {
+        res.send(`Hello from worker ${process.pid}`);
+    });
+
+    // Start the server
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}, handled by worker ${process.pid}`);
+    });
 }
-
-
-app.get('/',function(req,res){
-    doWork(5000) // 5 sec
-    res.send("hello");
-})
-
-
-app.listen(9000,()=>{
-    console.log("start server");
-})
